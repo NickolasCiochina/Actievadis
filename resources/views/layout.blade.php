88f5d245a09b2b5e1b5a24e6048882c8b9f6f1e5
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-09-23 09:09:16 +0200

    Created Layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activiteiten</title>
+</head>
+<body>
+    <!-- container -->
+     <div class="container">
+        <!-- navbar -->
+         <header>
+            <ul>
+                <li>Item1</li>
+                <li>Item2</li>
+                <li>Item3</li>
+            </ul>
+         </header>
+
+         @yield('content')
+        <!-- Footer -->
+         <footer>
+            <ul>
+                <li>Item1</li>
+                <li>Item2</li>
+                <li>Item3</li>
+            </ul>
+         </footer>
+        
+    </div>
+
+</body>
+</html>
