3eca420eb6ed6d65c57cd149537435f0998942e1
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-09-25 10:48:26 +0200

    Added logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Activiteiten</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <!-- container -->
      <div class="container">
         <!-- navbar -->
          <header>
+            <div class="logo">
+                <a href="/"> <image src="{{ Vite::asset('resources/images/logo_covadis_2016.png') }}" alt="logo"></a>
+            </div>
             <ul>
-                <li>Item1</li>
-                <li>Item2</li>
+                <li>Home</li>
+                <li>Activiteiten</li>
                 <li>Item3</li>
             </ul>
          </header>
