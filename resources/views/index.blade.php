2d8ffc9fcc99f90a405bdaa35f38023acea885cc
WJGCC <wesley.jansen4@student.graafschapcollege.nl>
2024-09-25 10:52:11 +0200

    Start index page, html structure and some css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/wjcss.css', 'resources/js/wjcss.js'])
 </head>
 <body>
+<div class="hero">
+        <div class="overlay">
+            <h1>Welkom bij Covadis planner</h1>
+        </div>
+    </div>
 
 </body>
 </html>
